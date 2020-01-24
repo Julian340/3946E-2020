3394c23fdd76639e8bdfe7f152fb89b9639d13bd
Bearcat202020 <jbarakat20@kentdenver.org>
2020-01-23 17:42:21 -0700

    auton changes

--- 3946E-Tournament/src/main.cpp
+++ 3946E-Tournament/src/main.cpp
@@ -26,7 +26,10 @@ vex::motor rightLift(vex::PORT16, vex::gearSetting::ratio18_1, false);
 vex::motor leftLift(vex::PORT7, vex::gearSetting::ratio18_1, true);
 
 vex::controller Controller1 = vex::controller();
-//vex::controller Controller2 = vex::controller();
+vex::controller Partner = vex::controller();
+
+float kP = .3;
+float kI = 0;
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -47,8 +50,8 @@ void deploy ( void ){
     leftDrive.rotateFor(-200,vex::rotationUnits::deg,30,vex::velocityUnits::pct, false);
           rightDrive.rotateFor(-200,vex::rotationUnits::deg,30,vex::velocityUnits::pct);
 
-    leftLift.rotateTo(300, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, false);
-    rightLift.rotateTo(300, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, false);
+    leftLift.rotateTo(350, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, false);
+    rightLift.rotateTo(350, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, false);
 
     //deploy rollers
     rollerLeft.rotateFor(-700,vex::rotationUnits::deg,100,vex::velocityUnits::pct, false);
@@ -74,20 +77,68 @@ void turnRight(double deg){
 
 }
 
+void piDrive (float distance, float max){
+
+    //do math to convert inches into degrees
+    float target = (distance) + leftDrive.rotation(vex::rotationUnits::deg);
+    float average = (leftDrive.rotation(vex::rotationUnits::deg) + rightDrive.rotation(vex::rotationUnits::deg))/2; 
+    float totalError = 0;
+    int counter = 0;
+
+   while(counter < 5){
+        
+        //gets the average value of both the left and right side
+        average = (leftDrive.rotation(vex::rotationUnits::deg) + rightDrive.rotation(vex::rotationUnits::deg))/2;
+        //get error by subtracting the average
+        float error = target - average;
+        totalError += error;
+
+        float pTerm = error * kP;
+        float iTerm = totalError * kI;
+
+        if(fabs(error) < 40){
+          counter ++;
+        }
+        else counter = 0;
+    
+        if(pTerm + iTerm > max){
+          leftDrive.spin(vex::directionType::fwd,max,vex::velocityUnits::pct);
+          rightDrive.spin(vex::directionType::fwd,max,vex::velocityUnits::pct);
+        }
+        else{
+        leftDrive.spin(vex::directionType::fwd,(pTerm + iTerm),vex::velocityUnits::pct);
+        rightDrive.spin(vex::directionType::fwd,(pTerm + iTerm),vex::velocityUnits::pct);
+   }  
+
+        Controller1.Screen.clearScreen();
+        // Controller1.Screen.setCursor(1,1);
+      
+        // Controller1.Screen.print("%f", error);
+        Controller1.Screen.setCursor(1,1);
+        Controller1.Screen.print("%d", counter);
+        vex::task::sleep(2);
+        
+         
+     }
+        
+
+
+
+}
+
 
 void redStack (){
           deploy();
           vex::task::sleep(1200);
           
           //start the rollers
-          rollerLeft.spin(vex::directionType::fwd, 60, vex::velocityUnits::pct);
-          rollerRight.spin(vex::directionType::fwd, 60, vex::velocityUnits::pct);
+          rollerLeft.spin(vex::directionType::fwd, 70, vex::velocityUnits::pct);
+          rollerRight.spin(vex::directionType::fwd, 70, vex::velocityUnits::pct);
 
           //drive forward
-          leftDrive.rotateFor(1150,vex::rotationUnits::deg,40,vex::velocityUnits::pct, false);
-          rightDrive.rotateFor(1150,vex::rotationUnits::deg,40,vex::velocityUnits::pct);
-          
-          vex::task::sleep(50);
+          piDrive(2150, 75);
+
+          vex::task::sleep(10);
 
           //stop rollers
           rollerLeft.stop(hold);
@@ -97,18 +148,27 @@ void redStack (){
           leftDrive.rotateFor(-70,vex::rotationUnits::deg,40,vex::velocityUnits::pct, false);
           rightDrive.rotateFor(-70,vex::rotationUnits::deg,40,vex::velocityUnits::pct);
           
+          vex::task::sleep(100);
+          rollerLeft.spin(vex::directionType::rev, 20, vex::velocityUnits::pct);
+          rollerRight.spin(vex::directionType::rev, 20, vex::velocityUnits::pct);
+
+          vex::task::sleep(700);
+
+          rollerLeft.stop(hold);
+          rollerRight.stop(hold);
+
           //turns to face the goal
-          turnRight(171);
+          turnRight(315);
           vex::task::sleep(200);
 
           //drive to goal -- uses time instead because we might not always reach same value
           leftDrive.spin(vex::directionType::fwd, 70, vex::velocityUnits::pct);
           rightDrive.spin(vex::directionType::fwd, 70, vex::velocityUnits::pct);
 
-          vex::task::sleep(1100);
+          vex::task::sleep(1700);
 
-          leftDrive.stop(hold);
-          rightDrive.stop(hold);
+          leftDrive.stop(coast);
+          rightDrive.stop(coast);
 
           //place dwon stack
           rotator.spin(vex::directionType::fwd, 40, vex::velocityUnits::pct); 
@@ -126,8 +186,8 @@ void redStack (){
           vex::task::sleep(200);
 
           //back up
-          leftDrive.rotateFor(-200,vex::rotationUnits::deg,40,vex::velocityUnits::pct, false);
-          rightDrive.rotateFor(-200,vex::rotationUnits::deg,40,vex::velocityUnits::pct);
+          leftDrive.rotateFor(-400,vex::rotationUnits::deg,40,vex::velocityUnits::pct, false);
+          rightDrive.rotateFor(-400,vex::rotationUnits::deg,40,vex::velocityUnits::pct);
 
           //stop rollers
           rollerLeft.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
@@ -218,66 +278,6 @@ void blueStack (){
 }
 
 
-//void blueStack(){
- //deploy();
-          //vex::task::sleep(1200);
-          
-          //start the rollers
-          //rollerLeft.spin(vex::directionType::fwd, 60, vex::velocityUnits::pct);
-          //rollerRight.spin(vex::directionType::fwd, 60, vex::velocityUnits::pct);
-
-          //drive forward
-          //leftDrive.rotateFor(1250,vex::rotationUnits::deg,40,vex::velocityUnits::pct, false);
-          //rightDrive.rotateFor(1250,vex::rotationUnits::deg,40,vex::velocityUnits::pct);
-          
-          //vex::task::sleep(500);
-
-          //stop rollers
-          //rollerLeft.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
-          //rollerRight.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
-
-          //drive back
-          //leftDrive.rotateFor(-70,vex::rotationUnits::deg,40,vex::velocityUnits::pct, false);
-          //rightDrive.rotateFor(-70,vex::rotationUnits::deg,40,vex::velocityUnits::pct);
-          
-          //turns to face the goal
-          //turnLeft(177);
-          //vex::task::sleep(200);
-
-          //drive to goal -- uses time instead because we might not always reach same value
-          //leftDrive.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-          //rightDrive.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-
-          //vex::task::sleep(1000);
-
-           //leftDrive.spin(vex::directionType::rev, 0, vex::velocityUnits::pct);
-          //rightDrive.spin(vex::directionType::rev, 0, vex::velocityUnits::pct);
-
-          //place dwon stack
-          //rotator.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct); 
-
-          //vex::task::sleep(2500);
-
-         //rotator.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct); 
-
-          //rollerLeft.spin(vex::directionType::rev, 20, vex::velocityUnits::pct);
-          //rollerRight.spin(vex::directionType::rev, 20, vex::velocityUnits::pct);
-
-          //vex::task::sleep(400);
-
-          //back up
-          //leftDrive.rotateFor(-200,vex::rotationUnits::deg,40,vex::velocityUnits::pct, false);
-          //rightDrive.rotateFor(-200,vex::rotationUnits::deg,40,vex::velocityUnits::pct);
-          
-//}
-
-
-/*void turn2 (double time){
-   leftDrive.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct); //(Axis3+Axis4)/2
-    rightDrive.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);//(Axis3-Axis4)/2        
-        
-}*/
-
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
 /*                              Autonomous Task                              */
@@ -286,7 +286,7 @@ void blueStack (){
 
 void autonomous( void ) {
  
-   deploy();
+   redStack();
 
 }
 
@@ -332,17 +332,19 @@ void usercontrol( void ) {
        
         
         //lift
-        if(Controller1.ButtonL1.pressing()){
+        if(Controller1.ButtonL2.pressing()){
+            
             rightLift.spin(vex::directionType::fwd, 60, vex::velocityUnits::pct);
             leftLift.spin(vex::directionType::fwd, 60, vex::velocityUnits::pct);
-            
+            rotator.spin(vex::directionType::rev, 60, vex::velocityUnits::pct);
             
         }
-        else if(Controller1.ButtonL2.pressing()){
+        else if(Controller1.ButtonL1.pressing()){
+            rotator.spin(vex::directionType::fwd, 40, vex::velocityUnits::pct);
             rightLift.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);  
             leftLift.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
-           
-        
+            
+
         }
         else {
          
@@ -352,7 +354,7 @@ void usercontrol( void ) {
         } 
 
         if(Controller1.ButtonUp.pressing()){
-          deploy();
+          redStack();
 
         }
     vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources. 
